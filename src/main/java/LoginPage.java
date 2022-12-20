1b4241ae5846b4f5eff4f32c668d818c4d150527
AndreyLinnik <av.linnik79@ya.ru>
2022-12-20 15:44:53 +0300

    add some methods

--- src/main/java/LoginPage.java
+++ src/main/java/LoginPage.java
@@ -1,17 +1,31 @@
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class LoginPage {
+    WebDriver driver;
+    WebDriverWait webDriverWait;
+    Actions actions;
+
+
     @FindBy(id = "email")
     public WebElement emailField;
     @FindBy(id = "passwd")
-    public WebElement password;
-    @FindBy(id = "SubmitLogin");
+    public WebElement passwordField;
+    @FindBy(id = "SubmitLogin")
     public WebElement submitLoginButton;
+    public  void login(String email, String password){
+emailField.sendKeys(email);
+passwordField.sendKeys(password);
+submitLoginButton.click();
+
+    }
 
     public LoginPage(WebDriver driver) {
+        this.driver=driver;
         PageFactory.initElements(driver, this);
     }
 }
--- src/test/java/AutoTest.java
+++ src/test/java/AutoTest.java
@@ -1,10 +1,33 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class AutoTest {
     WebDriver driver;
+
+    @BeforeAll
+    static  void registerDriver(){
+        WebDriverManager.chromedriver().setup();
+
+
+    }
+    @BeforeEach
+    void initDriver(){
+        driver=new ChromeDriver();
+    }
+
     @Test
     void addToCartTest(){
-        new LoginPage(driver).emailField
+        driver.get("http://automationpractice.com/index.php?controller=authentication&back=my-account");
+        new LoginPage(driver).login("spartalex93@test.test", "123456");
     }
+    @AfterEach
+    void teardown(){
+        driver.quit();
+    }
+
 }
